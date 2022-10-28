03ef2712e3d24b901cea0ed7264bb3bc97c2a335
tairongarrido <tgc2023@gmail.com>
2022-10-28 17:09:52 -0300

    fixs respect to rubocop and coverage

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -10,9 +10,19 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test 'post new product' do
     assert_difference 'Store.count' do
       post stores_url,
-           params: { store: { price: 2, category: 'Bebestible', weight_volume: 3, unit_value: 'Kg' } }
+           params: { store: { price: 2, category: 'Bebestible', weight_volume: 3, unit_value: 'Litros' } }
     end
   end
+
+  test 'post new product with incorrect combination category and unit' do
+    assert_no_changes 'Store.count' do
+      post stores_url,
+           params: { store: { price: 2, category: 'Bebestible', weight_volume: 3, unit_value: 'Kg' } }
+           post stores_url,
+           params: { store: { price: 2, category: 'Comestibles', weight_volume: 3, unit_value: 'Litros' } }
+    end
+  end
+  
 
   test 'post new product with invalid paremeters' do
     assert_no_changes 'Store.count' do
