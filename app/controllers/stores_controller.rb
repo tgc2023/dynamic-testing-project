a226e6cee799055babb7e8f3839d9877f3c03f53
brayanmoreno <bbmoreno@uc.cl>
2022-10-28 17:16:58 -0300

    crud completo

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 class StoresController < ApplicationController
+  before_action :set_store, only: [:show, :edit, :update, :destroy]
+
   def index
     @products = Store.all
     respond_to do |format|
       format.html
     end
+  end
+
+  def show
   end
 
   def new
@@ -21,12 +26,32 @@ class StoresController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @product.update(store_params)
+      redirect_to stores_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to stores_path
+  end
+
   def list_by_category
     @category = params[:category]
     @filter = Store.where(category: @category)
   end
 
   private
+
+  def set_store
+    @product = Store.find(params[:id])
+  end
 
   def store_params
     params.require(:store).permit(:price, :category, :weight_volume, :unit_value)
