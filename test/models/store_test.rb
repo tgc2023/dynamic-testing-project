99c924b140c2831e94f5f3690d5140ce6a162f54
tairongarrido <tgc2023@gmail.com>
2022-10-28 17:09:52 -0300

    fixs respect to rubocop and coverage

--- test/models/store_test.rb
+++ test/models/store_test.rb
@@ -7,8 +7,12 @@ class StoreTest < ActiveSupport::TestCase
     Store.destroy_all
   end
 
-  test 'Store creada con parametros validos' do
+  test 'Store with invalid parameters' do
     product = Store.create(category: 0, price: 10, weight_volume: 10, unit_value: 0)
+    assert_equal(false, product.valid?)
+  end
+  test 'Store with valid parameters' do
+    product = Store.create(category: 1, price: 10, weight_volume: 10, unit_value: 0)
     assert_equal(true, product.valid?)
   end
 
