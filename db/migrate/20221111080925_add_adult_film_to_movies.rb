6fb3f71352531cebec6b67f695efa08b589a5ce0
brayanmoreno <bbmoreno@uc.cl>
2022-11-11 06:43:27 -0300

    migration pelicula

---
+++ db/migrate/20221111080925_add_adult_film_to_movies.rb
@@ -0,0 +1,5 @@
+class AddAdultFilmToMovies < ActiveRecord::Migration[7.0]
+  def change
+    add_column :movies, :adult_film, :boolean, default: false, null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_15_204857) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_11_084533) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,6 +58,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_15_204857) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.boolean "adult_film", default: false, null: false
+    t.integer "idiom", default: 0, null: false
   end
 
   create_table "reservas", force: :cascade do |t|
