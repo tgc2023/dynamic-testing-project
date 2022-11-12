8b563ecaacb09651888c26d949c321fc779089c9
tairongarrido <tgc2023@gmail.com>
2022-11-11 21:06:47 -0300

    filter spec

--- spec/filter_spec.rb
+++ spec/filter_spec.rb
@@ -4,6 +4,9 @@ require 'rails_helper'
 require 'spec_helper'
 
 describe 'Navigation', type: :feature do
+  before(:each) do
+    @movie = Movie.create(title: 'movie one', idiom: 'Espanol', adult_film: true)
+  end
   scenario 'basic navigation' do
     visit '/'
     # save_and_open_page
