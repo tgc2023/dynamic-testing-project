ca4686bb032dad5a2a6a803f0b8acdbe14b0eda8
tairongarrido <tgc2023@gmail.com>
2022-11-11 21:03:34 -0300

    configure rspec

---
+++ spec/filter_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'spec_helper'
+
+describe 'Navigation', type: :feature do
+  scenario 'basic navigation' do
+    visit '/'
+    # save_and_open_page
+  end
+end
